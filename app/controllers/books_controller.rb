0a95d6dd6268927b57bc631d32884d2aba6ee414
SatoK4 <135345700+SatoK4@users.noreply.github.com>
2023-06-28 14:16:32 +0000

    [Add] createとupdate字のフラッシュメッセージを追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,13 @@
 class BooksController < ApplicationController
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render :new
+    end
   end
 
   def index
@@ -21,8 +25,10 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book.id)
+    end
   end
 
   def destroy
